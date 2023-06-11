8be2b9b0b3daa8d90a88c38e00d345c714b19f0c
Majost <57063653+Maxi-S-Dev@users.noreply.github.com>
2023-06-11 18:20:15 +0200

    fixed Windows Styling

--- Contractor/Contractor/App.xaml.cs
+++ Contractor/Contractor/App.xaml.cs
@@ -10,4 +10,17 @@ public partial class App : Application
 
 		Shell.Current.GoToAsync("//MainPage");
 	}
+
+#if WINDOWS
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var Window = base.CreateWindow(activationState);
+
+		Window.Width= 505;
+
+		Window.Height = 700;
+
+		return Window;
+    }
+#endif
 }
--- Contractor/Contractor/Platforms/Windows/App.xaml.cs
+++ Contractor/Contractor/Platforms/Windows/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
+using Windows.Security.EnterpriseData;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -17,6 +18,8 @@ public partial class App : MauiWinUIApplication
 	public App()
 	{
 		this.InitializeComponent();
+	
+	
 	}
 
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
