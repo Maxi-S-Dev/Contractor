ae04d37a4edf9e98b6af7ff268f14dcf620db67e
Max <max.03.strigl@hotmail.com>
2023-06-19 20:08:30 +0200

    added a workaround for the bugged Picker and updated light mode colors

--- Contractor/Contractor/View/SettingsPage.xaml.cs
+++ Contractor/Contractor/View/SettingsPage.xaml.cs
@@ -1,10 +1,63 @@
 using Microsoft.Maui.Handlers;
+using System.Diagnostics;
+using Contractor.ViewModel;
+using Contractor.Enums;
 
 namespace Contractor.View;
 public partial class SettingsPage : ContentPage
 {
+    SettingsViewModel vm;
+
+    Picker p;
+
 	public SettingsPage()
 	{
 		InitializeComponent();
-	}	
+
+        vm = new SettingsViewModel();
+        BindingContext = vm;
+
+        Application.Current.RequestedThemeChanged += (s, e) =>
+        {
+            p = null;
+            LoadPicker();
+        };
+
+        LoadPicker();
+    }
+
+    private void LoadPicker()
+    {
+        p = new();
+        p.MinimumWidthRequest = 120;
+        p.MinimumHeightRequest = 40;
+        p.VerticalOptions = LayoutOptions.Center;
+
+        if (Application.Current.UserAppTheme == AppTheme.Dark) p.Background = Color.FromArgb("#ff1e1e1e");
+        else p.Background = Color.FromArgb("#fff8fafd");
+        p.TextColor = Colors.White;
+        DesignGrid.Add(p, 2, 0);
+        p.ItemsSource = vm.DesignStates;
+        p.SelectedItem = vm.CurrentDesign;
+
+
+        p.SelectedIndexChanged += (s, e) =>
+        {
+            if(p.SelectedItem.ToString() == Design.Light.ToString())
+            {
+                vm.CurrentDesign = Design.Light;
+                return;
+            }
+            if (p.SelectedItem.ToString() == Design.Dark.ToString())
+            {
+                vm.CurrentDesign = Design.Dark;
+                return;
+            }
+            if (p.SelectedItem.ToString() == Design.Default.ToString())
+            {
+                vm.CurrentDesign = Design.Default;
+                return;
+            }
+        };
+    }
 }
