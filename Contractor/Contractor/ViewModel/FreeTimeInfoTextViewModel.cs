7a0b4f813cc815abcdb4fca29ed1475184a2e4df
Max <max.03.strigl@hotmail.com>
2023-06-21 15:05:31 +0200

    Main Page info Text finished

---
+++ Contractor/Contractor/ViewModel/FreeTimeInfoTextViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Contractor.Services;
+using Contractor.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Contractor.ViewModel
+{
+    public class FreeTimeInfoTextViewModel : ViewModelBase
+    {
+        DataStore ds = Application.Current.Handler.MauiContext.Services.GetService(typeof(DataStore)) as DataStore;
+        public string Text
+        {
+            get
+            {
+                int hours = (int)(ds.FreeSeconds / 3600);
+                int mins = (int)((ds.FreeSeconds % 3600) / 60);
+
+                return $"{hours}h and {mins}mins";
+            }
+        }
+
+        public FreeTimeInfoTextViewModel()
+        {
+            MainTimer.Dispatcher.Tick += (s, e) =>
+            {
+                OnPropertyChanged(nameof(Text));
+            };
+        }
+    }
+}
